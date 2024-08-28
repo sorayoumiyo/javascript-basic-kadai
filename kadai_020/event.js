e1cb5a4285efc2570bbe2b62ac9c4bed270fc64a
sorayoumiyo <miwamiwabiwako@gmail.com>
2024-08-29 01:28:24 +0900

    ファイルを追加

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('text');
+
+const outptBtn = document.getElementById('btn');
+
+outptBtn.addEventListener('click', () => {
+    text.textContent = 'ボタンをクリックしました';
+    
+});
